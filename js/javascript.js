f20da3447f258e24351a0f4076bbea12b51ea30f
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-10-15 14:28:06 -0500

    Additions:
    renderItemTemp()
    called the function clearTemp to remove current HTML before a new one is generated
    
    clearTemp()
    created function to clear any innerHTML from the element passed as a parameter

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,25 @@
 function renderItemTemp() {
+    var storeItemUl = document.querySelector("div.store-items ul");
+    clearTemp(storeItemUl);
     var source = document.getElementById("store-item-temp").innerHTML;
     var template = Handlebars.compile(source);
     var html = template(PAGE_DATA.storeItems);
-    document.querySelector("div.store-items ul").insertAdjacentHTML("beforeend", html)
+    storeItemUl.insertAdjacentHTML("beforeend", html);
 }
+
+function clearTemp(element) {
+    if (element.innerHTML) {
+        element.innerHTML = "";
+    }
+}
+// want to add event to each button that decreases the stock count everytime the button is clicked
+/* ideas:
+- make the stock count into a template;
+- from each button target the stock for each list item
+- use handlebars helper functions to add the event to the button(side effect) 
+  real effect returns a string to use as the tooltip; ( (not) bingo)
+- iterate through PAGE_DATA to find the correct name of the item the button is under 
+ */
+// function addEvent(i) {
+//     i.addEventListener("click", rentItem)
+// }
