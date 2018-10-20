54b79dde5af268c9abf32a8c73e3db0f7b305e97
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-10-19 22:57:52 -0500

    added a new key to PAGE_DATA called receipt that will hold a list of objects; set the initial value to an empty list

--- js/data.js
+++ js/data.js
@@ -2,22 +2,26 @@ var PAGE_DATA = {
     storeItems: [
         {
             name: "Saitama",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper. Adipiscing diam donec adipiscing tristique risus nec.",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper. Adipiscing diam donec adipiscing tristique risus nec.",
             price: "40.00",
             stock: "5"
             // image: images/image_url
         },
         {
             name: "Calculator",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper. Adipiscing diam donec adipiscing tristique risus nec.",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper. Adipiscing diam donec adipiscing tristique risus nec.",
             price: "50.00",
             stock: "5"
         },
         {
             name: "Spongebob",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper. Adipiscing diam donec adipiscing tristique risus nec.",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper. Adipiscing diam donec adipiscing tristique risus nec.",
             price: "25.00",
             stock: "5"
-        },
-    ]
-}
+        }
+    ],
+    receipt: []
+};
