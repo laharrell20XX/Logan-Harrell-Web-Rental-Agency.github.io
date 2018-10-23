83f0c35a14033bcde29811cc7a43f8a1cfa5dbaf
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-10-23 04:12:05 -0500

    replaced the placeholder data with real data

--- js/data.js
+++ js/data.js
@@ -3,24 +3,54 @@ var PAGE_DATA = {
         {
             name: "Saitama",
             description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper. Adipiscing diam donec adipiscing tristique risus nec.",
+                "A bright yellow suit with red boots, gloves and a white cape and belt. ONE PUUNCH! *Bald cap included*",
             price: "40.00",
-            stock: "5"
-            // image: images/image_url
+            stock: "5",
+            imageUrl: "images/Saitama.png"
         },
         {
-            name: "Calculator",
+            name: "MIB",
             description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper. Adipiscing diam donec adipiscing tristique risus nec.",
+                "Take on aliens in style with this classic sleek suit and black shades. *zap* Forget that last sentence. This is just a normal suit. ",
             price: "50.00",
-            stock: "5"
+            stock: "5",
+            imageUrl: "images/boy_in_black.jpg"
         },
         {
-            name: "Spongebob",
+            name: "Scary Ghost",
             description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper. Adipiscing diam donec adipiscing tristique risus nec.",
+                "When all else fails, dress up as a ghost. Better than nothing right? Don't scare anybody TOO hard out there...",
             price: "25.00",
-            stock: "5"
+            stock: "5",
+            imageUrl: "images/ghost-costume.jpg"
+        },
+        {
+            name: "Varia Suit",
+            description: "Don the suit of this legendary bounty hunter and become feared by space pirates everywhere!  Additional costume pieces can be found in the field.",
+            price: "70.00",
+            stock: 2,
+            imageUrl: "images/varia-suit.jpg"
+        },
+        {
+            name: "Xenomorph",
+            description: "Become the terror of space and earth alike in this terrifyingly real xenomorph costume. Those pesky humans never see this one coming, unless they have a motion tracker.",
+            price: "100.00",
+            stock: 1,
+            imageUrl: "images/xenomorph.jpg"
+        },
+        {
+            name: "Turtle",
+            description: "What, this isn't a costume! It's just a picture of an actual turtle.  The page probably got hacked by some dang kids or something. Unbelievable...",
+            price: "25.00",
+            stock: 0,
+            imageUrl: "images/turtle-dog.jpg"
+        },
+        {
+            name: "Godzilla",
+            description: "It's the world's favorite (or least favorite depending on his mood) radioactive reptile!  Feel free to knock over a couple lego sets for that authentic feel.",
+            price: "80.00",
+            stock: 10,
+            imageUrl: "images/godzilla.jpg"
         }
     ],
     receipt: []
